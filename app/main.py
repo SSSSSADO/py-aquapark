80f6bc5b64c79c5013c8bb1fed90df90298b605c
SSSSSADO <grushkovik.danil@gmail.com>
2025-11-24 13:01:35 +0300

    create descriptor IntegerRange

--- app/main.py
+++ app/main.py
@@ -1,8 +1,24 @@
+from __future__ import annotations
 from abc import ABC
 
 
 class IntegerRange:
-    pass
+    def __init__(self, min_amount: int, max_amount: int):
+        self.min_amount = min_amount
+        self.max_amount = max_amount
+
+    def __set_name__(self, owner: type, name: str) -> None:
+        self.protected_name = "_" + name
+
+    def __get__(self, instance: object, owner: type) -> object:
+        return getattr(instance, self.protected_name)
+
+    def __set__(self, instance: object, value: object) -> None:
+        if not isinstance(value, int):
+            raise TypeError()
+        if not (self.min_amount <= value <= self.max_amount):
+            raise ValueError()
+        setattr(instance, self.protected_name, value)
 
 
 class Visitor:
